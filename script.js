751db12c061dc1aebb4c53f06445afa16e9f7d6c
andreapanico12 <andreapanico12@gmail.com>
2025-03-26 15:24:55 +0100

    Add error handling to getChefBirthday function for recipe and chef retrieval

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ async function fetchJason(url){
 }
 
 async function getChefBirthday(id) {
-  const recipe = await fetchJason(`https://dummyjson.com/recipes/${id}`)
-  let chefId = recipe.userId;
-  const chef = await fetchJason(`https://dummyjson.com/users/${chefId}`)
-  let birthday = chef.birthDate;
 
+  let recipe
+  let chefId
+  let chef
+  let birthday
+  // NB:try/catch creano uno scope locale
+  try{
+    recipe = await fetchJason(`https://dummyjson.com/recipes/${id}`)
+    chefId = recipe.userId;
+  } 
+  catch(error) {
+    throw new Error(`Non posso trovare la ricetta con ID: ${id}`)
+  }
+  try{
+    chef = await fetchJason(`https://dummyjson.com/users/${chefId}`)
+    birthday = chef.birthDate;
+  }
+  catch(error){
+    console.log("Entrato nel secondo catch!")
+    throw new Error(`Non posso trovare lo chef con ID :${chefId}`)
+  }
+  // posizionare il return all'interno del finally non permetteva al secondo catch di fare il throw dell'errore.
   return birthday
 
 }
