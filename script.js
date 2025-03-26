25a5e159ea91d034d1e18d59b14c6e2f5604aca7
andreapanico12 <andreapanico12@gmail.com>
2025-03-26 14:54:28 +0100

    Update README, add HTML structure, and implement getChefBirthday function

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+async function getChefBirthday(id) {
+  const responseRicetta = await fetch(`https://dummyjson.com/recipes/${id}`)
+  const ricetta = await responseRicetta.json();
+  let chefId = ricetta.userId;
+  const responseChef = await fetch(`https://dummyjson.com/users/${chefId}`)
+  const chef = await responseChef.json()
+  
+  dataNascitaChef = chef.birthDate;
+
+  return dataNascitaChef
+
+}
+
+(async () => {
+  const dataNascitaChef = await getChefBirthday(1);
+  console.log(dataNascitaChef)
+}) ();
